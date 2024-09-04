f01210434f83dab7423c5544b973629f7a45ecfa
PRASANNARAJEN <91543995+PRASANNARAJEN@users.noreply.github.com>
2024-09-04 19:54:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://chain-forge-forntend-griz.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.options('*', cors({
     origin: 'https://chain-forge-forntend-griz.vercel.app',
